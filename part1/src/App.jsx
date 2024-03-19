afeb3a7e93be78f4329b51bfe02c95c34327c69c
algarfer <algarferx@proton.me>
2024-03-19 17:46:14 +0100

    Ex 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -19,28 +19,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-    const course = "Half Stack application development"
-    const parts = [
-        {
-            name: "Fundamentals of React",
-            exercises: 10
-        },
-        {
-            name: "Using props to pass data",
-            exercises: 7
-        },
-        {
-            name: "State of a component",
-            exercises: 14
-        }
-    ]
-
+    const course = {
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7
+            },
+            {
+                name: "State of a component",
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header title={course} />
-            <Content data={parts} />
-            <Total values={parts} />
+            <Header title={course.name} />
+            <Content data={course.parts} />
+            <Total values={course.parts} />
         </div>
     )
 }
