49ec92c7efe3657057ea6ac22b8e2b46b897d138
algarfer <algarferx@proton.me>
2024-03-23 21:20:41 +0100

    Ex 2.3-2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Header = (props) => {
-    return <h1>{props.title}</h1>
+    return <h2>{props.title}</h2>
 }
 
 const Part = (props) => {
@@ -15,7 +15,7 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-    return <p>Number of exercises {props.values.reduce((acc, current) => acc + current.exercises, 0)}</p>
+    return <p><b>Number of exercises {props.values.reduce((acc, current) => acc + current.exercises, 0)}</b></p>
 }
 
 const Course = ({course}) => {
@@ -29,34 +29,57 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3
-            },
-            {
-                name: "Redux",
-                exercises: 11,
-                id: 4
-            }
-        ]
-    }
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
 
-    return <Course course={course} />
+    return (
+        <>
+            <h1>Web development curriculum</h1>
+            {courses.map(course => <Course key={course.id} course={course} />)}
+        </>
+    )
 }
 
 export default App
