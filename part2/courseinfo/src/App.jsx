187f2150f8b6bd9b767bcde26654efdf99d29415
algarfer <algarferx@proton.me>
2024-03-23 21:22:15 +0100

    Ex 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,32 +1,4 @@
-const Header = (props) => {
-    return <h2>{props.title}</h2>
-}
-
-const Part = (props) => {
-    return <p>{props.exercise.name} {props.exercise.exercises}</p>
-}
-
-const Content = (props) => {
-    return (
-        <div>
-            {props.data.map(item => <Part key={item.id} exercise={item} />)}
-        </div>
-    )
-}
-
-const Total = (props) => {
-    return <p><b>Number of exercises {props.values.reduce((acc, current) => acc + current.exercises, 0)}</b></p>
-}
-
-const Course = ({course}) => {
-    return (
-        <div>
-            <Header title={course.name} />
-            <Content data={course.parts} />
-            <Total values={course.parts} />
-        </div>
-    )
-}
+import Course from "./Course.jsx";
 
 const App = () => {
     const courses = [
