54625f5fcecd89e576da07be565f2802cd6b1d56
algarfer <algarferx@proton.me>
2024-03-28 03:51:25 +0100

    Ex 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import Persons from "./components/Persons.jsx";
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [filter, setFilter] = useState('')
@@ -17,6 +13,13 @@ const App = () => {
     const handleInputName = (e) => setNewName(e.target.value)
     const handleInputNumber = (e) => setNewNumber(e.target.value)
     const handleFilter = (e) => setFilter(e.target.value)
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons")
+            .then(response => {
+                setPersons(response.data)
+            })
+    }, []);
 
     const handleForm = (e) => {
         e.preventDefault()
