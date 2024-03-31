da7f14f94742f0c501e88d5693259eccbc33485c
algarfer <algarferx@proton.me>
2024-03-31 03:51:12 +0200

    Ex 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,54 +1,65 @@
-import { useState, useEffect } from 'react'
+import {useState, useEffect} from 'react'
 import Filter from "./components/Filter.jsx";
 import PersonForm from "./components/PersonForm.jsx";
 import Persons from "./components/Persons.jsx";
-import axios from "axios";
+import contacts from "./services/contacts";
 
 const App = () => {
-    const [persons, setPersons] = useState([])
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
-    const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
-    const handleInputName = (e) => setNewName(e.target.value)
-    const handleInputNumber = (e) => setNewNumber(e.target.value)
-    const handleFilter = (e) => setFilter(e.target.value)
+  const handleInputName = (e) => setNewName(e.target.value)
+  const handleInputNumber = (e) => setNewNumber(e.target.value)
+  const handleFilter = (e) => setFilter(e.target.value)
 
-    useEffect(() => {
-        axios.get("http://localhost:3001/persons")
-            .then(response => {
-                setPersons(response.data)
-            })
-    }, []);
+  useEffect(() => {
+    contacts
+      .getAll()
+      .then(p => {
+        setPersons(p)
+      })
+  }, []);
 
-    const handleForm = (e) => {
-        e.preventDefault()
-        if (persons.find(({name}) => name === newName)) {
-            alert(`${newName} is already added to phonebook`)
-            return
-        }
-        setPersons(persons.concat({ name: newName, number: newNumber, id: persons.length + 1}))
+  const handleForm = (e) => {
+    e.preventDefault()
+    if (persons.find(({name}) => name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+    const person = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
+    contacts
+      .create(person)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
         setFilter('')
-    }
+      })
+  }
 
-    return (
-        <div>
-            <h1>Phonebook</h1>
-            <Filter handler={handleFilter} filter={filter} />
-            <h2>Add a new</h2>
-            <PersonForm
-                formHandler={handleForm}
-                nameHandler={handleInputName}
-                name={newName}
-                numberHandler={handleInputNumber}
-                number={newNumber}
-            />
-            <h2>Numbers</h2>
-            <Persons persons={persons} filter={filter} />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <Filter handler={handleFilter} filter={filter} />
+      <h2>Add a new</h2>
+      <PersonForm
+        formHandler={handleForm}
+        nameHandler={handleInputName}
+        name={newName}
+        numberHandler={handleInputNumber}
+        number={newNumber}
+      />
+      <h2>Numbers</h2>
+      <Persons persons={persons} filter={filter} />
+    </div>
+  )
 }
 
 export default App
