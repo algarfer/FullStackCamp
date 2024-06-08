e61ac01cd3e21b1b179ffc509b452f5783a292d3
algarfer <algarferx@proton.me>
2024-06-08 23:30:33 +0200

    Ex 2.19

--- part2/countries/src/components/Search.jsx
+++ part2/countries/src/components/Search.jsx
@@ -3,6 +3,7 @@ import Display from "./Display";
 
 const Search = ({data}) => {
   const [search, setSearch] = useState("");
+  const [display, setDisplay] = useState(null);
 
   const searchList = () => {
     const current =  data
@@ -14,10 +15,21 @@ const Search = ({data}) => {
           .toLowerCase()))
     if (current.length === 1) return <Display name={current[0].name.common} />
     if (current.length >= 10) return <p>Too many matches, specify another filter</p>
-    return <ul>{ current.map(d => <li key={d.name.common}>{d.name.common}</li>) }</ul>
+    return <ul>
+      {
+        current.map((d, i) =>
+          <li key={d.name.common}>
+            {d.name.common}
+            <button onClick={() => setDisplay(<Display name={current[i].name.common} />)}>
+              Show
+            </button>
+          </li>)
+      }
+    </ul>
   }
 
   return (
+    display ||
     <div>
       <div style={{display: "flex", alignItems: "center", gap: "1rem"}}>
         <p>find countries</p>
